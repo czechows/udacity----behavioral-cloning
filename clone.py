4a9557f2c165af193fa51abb82657394b62b6a45
czechows <czechovs@gmail.com>
2017-05-27 22:54:04 +0200

    Trying VGG

--- clone.py
+++ clone.py
@@ -34,36 +34,36 @@ model.add(Cropping2D( cropping((70,25), (1,1)), input_shape=(160,320,3) ))
 model.add(Lambda(lambda x: x / 255.0 - 0.5)
 
 # VGG: Block 1
-model.add(Conv2D(64, 3, 3, activation='relu', border_mode='same', name='block1_conv1'))
-model.add(Conv2D(64, 3, 3, activation='relu', border_mode='same', name='block1_conv2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
+#model.add(Conv2D(64, 3, 3, activation='relu', border_mode='same', name='block1_conv1'))
+#model.add(Conv2D(64, 3, 3, activation='relu', border_mode='same', name='block1_conv2'))
+#model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool'))
 
 # Block 2
-model.add(Conv2D(128, 3, 3, activation='relu', border_mode='same', name='block2_conv1'))
-model.add(Conv2D(128, 3, 3, activation='relu', border_mode='same', name='block2_conv2'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
+#model.add(Conv2D(128, 3, 3, activation='relu', border_mode='same', name='block2_conv1'))
+#model.add(Conv2D(128, 3, 3, activation='relu', border_mode='same', name='block2_conv2'))
+#model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool'))
 
 # Block 3
-model.add(Conv2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv1'))
-model.add(Conv2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv2'))
-model.add(Conv2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
+#model.add(Conv2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv1'))
+#model.add(Conv2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv2'))
+#model.add(Conv2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv3'))
+#model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool'))
 
 # Block 4
-model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv1'))
-model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv2'))
-model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
+#model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv1'))
+#model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv2'))
+#model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv3'))
+#model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool'))
 
 # Block 5
-model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv1'))
-model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv2'))
-model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv3'))
-model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
+#model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv1'))
+#model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv2'))
+#model.add(Conv2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv3'))
+#model.add(MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool'))
 
 model.add(Flatten(), name='flatten')
-model.add(Dense(4096), activation='relu', name='fc1')
-model.add(Dense(4096), activation='relu', name='fc2')
+#model.add(Dense(4096), activation='relu', name='fc1')
+#model.add(Dense(4096), activation='relu', name='fc2')
 model.add(Dense(1), name='predictions')
 
 model.compile(loss='mse', optimizer='adam')
